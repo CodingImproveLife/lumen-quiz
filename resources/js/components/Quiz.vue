b5d2695ab445b81b8f16d948d30f1c46a0c1211f
CodingImproveLife <web-coder@tuta.io>
2021-12-07 23:59:26 +0100

    Changed quiz component

--- resources/js/components/Quiz.vue
+++ resources/js/components/Quiz.vue
@@ -3,6 +3,22 @@
         <h1>{{ quiz.name }}</h1>
         <div>{{ quiz.description }}</div>
         <div>{{ quiz.category_id }}</div>
+        <ul v-for="question in quiz.questions">
+            <li>
+                {{ question.id }}
+            </li>
+            <li>
+                {{ question.title }}
+            </li>
+            <li>
+                {{ question.description }}
+            </li>
+                <ul v-for="answer in question.answers">
+                    <li>
+                        {{ answer }}
+                    </li>
+                </ul>
+        </ul>
     </div>
 </template>
 
