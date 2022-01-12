2f65b21e7b554b574bb08f1b62aed2d04da13aee
CodingImproveLife <web-coder@tuta.io>
2022-01-12 23:55:18 +0100

    Added login menu

--- resources/js/components/Navigation.vue
+++ resources/js/components/Navigation.vue
@@ -1,7 +1,30 @@
 <template>
-    <b-navbar toggleable="lg" type="dark" variant="secondary" sticky>
+    <b-navbar toggleable="lg" type="dark" variant="secondary" sticky class="mb-3">
         <b-navbar-brand :to="{ name: 'quizzes'}">
             <b-badge variant="light" class="text-uppercase p-2">Quiz</b-badge>
         </b-navbar-brand>
+
+        <b-navbar-nav class="ml-auto">
+            <div v-if="isGuest" class="d-flex">
+                <b-nav-item href="#">Sign in</b-nav-item>
+                <b-nav-item href="#">Sign up</b-nav-item>
+            </div>
+            <b-nav-item-dropdown right v-else>
+                <template #button-content>
+                    User
+                </template>
+                <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+            </b-nav-item-dropdown>
+        </b-navbar-nav>
     </b-navbar>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            isGuest: true
+        }
+    }
+}
+</script>
